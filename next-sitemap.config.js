3258f8582182902606d4c810c260f7fb9b4dc1f0
adarsh-bhatnagar <adarshb.best@gmail.com>
2025-11-06 01:25:04 +0530

    fix: Site indexing

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://prodomation.tech',  // your website URL
+  generateRobotsTxt: true,              // automatically generate robots.txt
+  changefreq: 'weekly',                 // optional: tells search engines how often pages change
+  priority: 0.7,                        // optional: priority for pages
+}
